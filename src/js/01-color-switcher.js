f8089bed6d7bbf7b758611767241b674a7452997
MikeTribrat <mike.lapp.ua@gmail.com>
2022-09-07 19:30:18 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const refs = {
+    body: document.body,
+    startButton: document.querySelector('button[data-start]'),
+    stopButton: document.querySelector('button[data-stop]'),
+};
 
+onPageLoad();
+
+let intervalId = null;
+
+refs.startButton.addEventListener('click', onStartClick);
+refs.stopButton.addEventListener('click', onStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartClick(e) {
+    switchButtons();
+
+    if (e.target.hasAttribute('data-start')) {
+    intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    }
+}
+
+function onStopClick(e) {
+    switchButtons();
+    clearInterval(intervalId);
+}
+
+function onPageLoad() {
+    refs.stopButton.disabled = true;
+}
+
+function switchButtons() {
+    refs.stopButton.disabled = !refs.stopButton.disabled;
+    refs.startButton.disabled = !refs.startButton.disabled;
+}
